523bcdafcfabd548eea284c774a4053717bf0ace
icarosg <icarodsouza662@gmail.com>
2024-06-23 00:34:32 -0300

    adding facade and updating controller and screen

--- src/main/system/model/Seller.java
+++ src/main/system/model/Seller.java
@@ -3,7 +3,7 @@ package main.system.model;
 import java.util.LinkedList;
 
 public class Seller extends User {
-    private LinkedList<Object> products = new LinkedList<>();
+    private LinkedList<Product> products = new LinkedList<>();
 
     public Seller(String name, String login, String password, String email, String address){
        super(name, login, password, email, address, 0);     //vendedor não tem forma de pagamento, já que não pode comprar
