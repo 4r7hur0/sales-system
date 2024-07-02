6e04e167b4f14faec27653655c3d5af808edde0b
Carlos Arthur <carlos17.fsa15@gmail.com>
2024-07-02 17:57:25 -0300

    stock completed

--- src/main/system/model/User.java
+++ src/main/system/model/User.java
@@ -2,6 +2,7 @@ package main.system.model;
 
 public class User {
     public static void main(String[] args) {
-        System.out.println("test");
+        ConcreteProductFactory factory;
+
     }
 }
